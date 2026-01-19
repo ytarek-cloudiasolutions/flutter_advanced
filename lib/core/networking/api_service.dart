f3bb1d6ce9ae4a7f203ca3c15b8bc0ce79278e2a
Youssef Tarek <yusuf2002@Youssefs-MacBook-Pro.local>
2026-01-19 15:04:30 +0200

    API Service

---
+++ lib/core/networking/api_service.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_complete_project/core/networking/api_constants.dart';
+import 'package:flutter_complete_project/features/login/data/models/login_request_body.dart';
+import 'package:flutter_complete_project/features/login/data/models/login_response_body.dart';
+import 'package:retrofit/retrofit.dart';
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: ApiConstants.apiBaseURL)
+abstract class ApiService {
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @POST(ApiConstants.login)
+  Future<LoginResponse> login(@Body() LoginRequestBody loginRequestBody);
+}
